194f97b9690c445dbf11e325aa41f8738245b7b8
utkarsh232005 <utkarshpatrikar@gmail.com>
2024-12-13 01:56:07 +0530

    added new loader

--- js/home.js
+++ js/home.js
@@ -2,4 +2,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Home page loaded.');
   });
-  
+  // script.js
+
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader');
+  const mainContent = document.getElementById('main-content');
+
+  // Add a slight delay for better UX (optional)
+  setTimeout(() => {
+    preloader.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 6000); // 6 second delay
+});
